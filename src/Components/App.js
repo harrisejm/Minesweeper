ebfd3fad1f473e0df9f1395a44a2a7867e2f71ce
e <harrisejm@gmail.com>
2019-06-03 17:55:38 -0700

    add images. Change beginnerBoardMain to an array of objects

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import '../Styles/App.css';
 import Board from './Board'
+import mine from '../assets/img/mine.jpeg';
+import one from '../assets/img/one.png';
+import two from '../assets/img/two.png';
+import three from '../assets/img/three.png';
+import four from '../assets/img/four.png';
+import five from '../assets/img/five.png';
+import six from '../assets/img/six.png';
+import seven from '../assets/img/seven.png';
+import eight from '../assets/img/eight.png';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,12 +27,12 @@ class App extends React.Component {
        [" "," "," "," "," "," "," "," "," "],
        [" "," "," "," "," "," "," "," "," "]
       ]
-      
     }
     this.small = this.small.bind(this);
     this.bombs = this.bombs.bind(this);
     this.newBoard = this.newBoard.bind(this);
     this.smallBoard = this.smallBoard.bind(this);
+    this.clickSquare = this.clickSquare.bind(this);
   }
 
   newBoard() {
@@ -33,7 +42,7 @@ class App extends React.Component {
     }
     return board;
   }
-
+  ///array of bomb locations
   bombs(arr, mineNumb){
     let tempArr = arr.slice();
     let arr1 = [];
@@ -45,6 +54,7 @@ class App extends React.Component {
        mineNumb--;
     }
       return arr1
+
   }
 
   smallBoard(){
@@ -54,6 +64,7 @@ class App extends React.Component {
     for (let i=0; i<10; i++) {
       boardArr[bombPosition[i]] = bombPosition[i];
     }
+    console.log(boardArr);
     return boardArr;
   }
 
@@ -61,72 +72,99 @@ class App extends React.Component {
     const bombArr = this.smallBoard();
     let finalBoard = [];
     let row = [];
+    let finalTest = [];
+    let test = [];
     let rowCount = 0;
     for (let i=0; i<81; i++) {
       if (bombArr[i]) {
-        row.push('X');
+        row.push({
+          space: "X",
+          image: null
+        });
+        test.push("X");
       } else {
-        row.push(" ");
+        row.push({
+          space: " ",
+          image: null
+        });
+      test.push(" ");
       }
       rowCount += 1;
       if (rowCount === 9) {
       finalBoard.push(row);
       row = [];
       rowCount = 0;
+
+
+      finalTest.push(test);
+      test = [];
       }
     }
+    console.log(finalTest);
     console.log(finalBoard);
-   // this.setState({beginnerBoardMain: finalBoard});
     return finalBoard;
   }
   componentWillMount(){
-   // this.small();
     this.bombNumb(this.small());
   }
 
   clickSquare(bomb){
-    
+    let beginnerBoardMain = this.state.beginnerBoardMain;
+    let arr =[one,two,three,four,five,six,seven,eight];
+
+
+    if (beginnerBoardMain[bomb[0]][bomb[1]].space === 'X') {
+      beginnerBoardMain[bomb[0]][bomb[1]].image = mine;
+  //    alert("GAME OVER" + bomb[0] +","+bomb[1]);
+    } else {
+      if (beginnerBoardMain[bomb[0]][bomb[1]].space > 0) {
+        beginnerBoardMain[bomb[0]][bomb[1]].image = arr[beginnerBoardMain[bomb[0]][bomb[1]].space-1]
+      }
+
+  //    alert(bomb[0] +","+bomb[1]);
+    }
+    this.setState({beginnerBoardMain: beginnerBoardMain})
   }
 
   bombNumb(board){
     for (let i=0; i<board.length; i++) {
       for (let j=0; j<board[0].length; j++) {
-        if (board[i][j] === "X") {
+        if (board[i][j].space === "X") {
           continue;
         }
         let count = 0;
         if(board[i-1]) {
-          if (board[j-1] && board[i-1][j-1] === "X") {
+          if (board[j-1] && board[i-1][j-1].space === "X") {
             count+=1;
           }
-          if (board[i-1][j] === "X") {
+          if (board[i-1][j].space === "X") {
             count+=1;
           }
-          if (board[j+1] && board[i-1][j+1] === "X") {
+          if (board[j+1] && board[i-1][j+1].space === "X") {
             count+=1;
           }
         }
         ////
-        if (board[j-1] && board[i][j-1] === "X") {
+        if (board[j-1] && board[i][j-1].space === "X") {
           count+=1;
         }
-        if (board[j+1] && board[i][j+1] === "X") {
+        if (board[j+1] && board[i][j+1].space === "X") {
           count+=1;
         }
         ////
         if (board[i+1]) {
-          if (board[j-1] && board[i+1][j-1] === "X") {
+          if (board[j-1] && board[i+1][j-1].space === "X") {
             count+=1;
           }
-          if (board[i+1][j] === "X") {
+          if (board[i+1][j].space === "X") {
             count+=1;
           }
-          if (board[j+1] && board[i+1][j+1] === "X") {
+          if (board[j+1] && board[i+1][j+1].space === "X") {
             count+=1;
           }
         }
         if (count > 0) {
-        board[i][j] = count;
+        board[i][j].space = count;
         }
       }
     }
@@ -159,6 +197,8 @@ class App extends React.Component {
   render(){
     return(
       <div>
+         <img src={mine}/>
+         <img src={one}/>
       <Board
         hello={this.state.hello}
         small={this.small}
--- src/Components/BeginnerBoard.js
+++ src/Components/BeginnerBoard.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import mine from '../assets/img/mine.jpeg';
 
 function BeginnerBoard(props){
 
 function genBoard(bombLocations) {
+  let space;
   const board = (
     <table>
       <tbody>
         {bombLocations.map((row,indexY)=>{
           let y = -1;
-          let x =-1;
+          let x = -1;
           y+=1;       
           return <tr key={indexY}>
             {row.map((square,indexX)=>{
             x+=1;
-            return  <td key={indexX} onClick={()=>props.clickSquare([indexY,indexX])}>{square}</td>
+            return  <td key={indexX} onClick={()=>props.clickSquare([indexY,indexX])}><img src={square.image}/>{square.space}</td>
             })}
           </tr>
         })}
